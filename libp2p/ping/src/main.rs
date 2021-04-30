b39fc1396d3ae46fce34ee01537652df208c84da
hwipl <33433250+hwipl@users.noreply.github.com>
2021-04-30 22:50:44 +0200

    add libp2p/ping
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

---
+++ libp2p/ping/src/main.rs
@@ -0,0 +1,61 @@
+// simple ping program based on libp2p ping example
+
+use futures::executor::block_on;
+use futures::prelude::*;
+use libp2p::ping::{Ping, PingConfig};
+use libp2p::swarm::Swarm;
+use libp2p::{identity, PeerId};
+use std::error::Error;
+use std::task::Poll;
+use std::time::Duration;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // create key and peer id
+    let local_key = identity::Keypair::generate_ed25519();
+    let local_peer_id = PeerId::from(local_key.public());
+    println!("Local peer id: {:?}", local_peer_id);
+
+    // create transport
+    let transport = block_on(libp2p::development_transport(local_key))?;
+
+    // create a ping network behaviour that pings every seconds
+    let behaviour = Ping::new(
+        PingConfig::new()
+            .with_keep_alive(true)
+            .with_interval(Duration::from_secs(1)),
+    );
+
+    // create swarm
+    let mut swarm = Swarm::new(transport, behaviour, local_peer_id);
+
+    // listen on loopback interface and random port.
+    swarm.listen_on("/ip6/::1/tcp/0".parse()?)?;
+    swarm.listen_on("/ip4/127.0.0.1/tcp/0".parse()?)?;
+
+    // connect to peer in first command line argument if present
+    if let Some(addr) = std::env::args().nth(1) {
+        let remote = addr.parse()?;
+        swarm.dial_addr(remote)?;
+        println!("Connecting to {}", addr)
+    }
+
+    // start main loop
+    let mut listening = false;
+    block_on(future::poll_fn(move |cx| loop {
+        match swarm.poll_next_unpin(cx) {
+            Poll::Ready(Some(event)) => println!("{:?}", event),
+            Poll::Ready(None) => return Poll::Ready(()),
+            Poll::Pending => {
+                if !listening {
+                    for addr in Swarm::listeners(&swarm) {
+                        println!("Listening on {}", addr);
+                        listening = true;
+                    }
+                }
+                return Poll::Pending;
+            }
+        }
+    }));
+
+    Ok(())
+}
