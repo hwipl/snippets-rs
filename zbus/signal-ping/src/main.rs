7a5ff943b8209983500feae3d7c35e8771713969
hwipl <33433250+hwipl@users.noreply.github.com>
2021-10-02 11:12:13 +0200

    add zbus/signal-ping
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

---
+++ zbus/signal-ping/src/main.rs
@@ -0,0 +1,51 @@
+use async_std::task;
+use futures_util::stream::StreamExt;
+use std::time::Duration;
+use zbus::{dbus_proxy, Connection, Result};
+
+#[dbus_proxy(
+    default_service = "org.ping.Ping",
+    interface = "org.ping.Ping",
+    default_path = "/org/ping/Ping"
+)]
+trait Ping {
+    #[dbus_proxy(signal)]
+    fn ping(&self, ping: &str) -> Result<()>;
+}
+
+/// periodically send ping signal
+async fn send_ping(conn: Connection) -> Result<()> {
+    loop {
+        task::sleep(Duration::from_secs(1)).await;
+        conn.emit_signal(
+            None::<()>,
+            "/org/ping/Ping",
+            "org.ping.Ping",
+            "Ping",
+            &"PING",
+        )
+        .await?;
+    }
+}
+
+#[async_std::main]
+async fn main() -> Result<()> {
+    // connect to session bus and request well known name
+    let conn = Connection::session().await?;
+    conn.request_name("org.ping.Ping").await?;
+
+    // create proxy and ping signal stream
+    let proxy = PingProxy::new(&conn).await?;
+    let mut ping_stream = proxy.receive_ping().await?;
+
+    // spawn task that sends ping signals
+    task::spawn(send_ping(conn));
+
+    // handle incoming ping signals
+    while let Some(signal) = ping_stream.next().await {
+        let args = signal.args()?;
+        println!("{}", args.ping());
+    }
+
+    Ok(())
+}
