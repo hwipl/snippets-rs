8f13b02fa17a287f889376cd0a0b9c0f8e5eef2c
hwipl <33433250+hwipl@users.noreply.github.com>
2021-10-25 21:56:54 +0200

    clap/derive: update for latest clap
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- clap/derive/src/main.rs
+++ clap/derive/src/main.rs
@@ -1,11 +1,10 @@
-use clap::{AppSettings, Clap};
+use clap::Parser;
 
 const VERSION: &str = "0.1.0";
 
 /// Tests clap command line argument parsing
-#[derive(Clap)]
+#[derive(Parser)]
 #[clap(version = VERSION)]
-#[clap(setting = AppSettings::ColoredHelp)]
 struct Args {
     /// Greeting
     #[clap(short, long, default_value = "hi")]
