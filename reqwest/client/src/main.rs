03c4d48989d9d2632823cc8b67ab248a55df45ae
hwipl <33433250+hwipl@users.noreply.github.com>
2022-07-30 17:33:17 +0200

    reqwest/client: add request command line parameter
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- reqwest/client/src/main.rs
+++ reqwest/client/src/main.rs
@@ -3,19 +3,28 @@ use std::time::Duration;
 
 #[async_std::main]
 async fn main() -> Result<()> {
-    // get url from command line
-    let addr = match std::env::args().nth(1) {
-        Some(addr) => addr,
-        None => "https://www.rust-lang.org".to_string(),
-    };
-
     // create client
     let client = reqwest::ClientBuilder::new()
         .timeout(Duration::from_secs(5))
         .build()?;
 
+    // get url from command line
+    let addr = match std::env::args().nth(2) {
+        Some(addr) => addr,
+        None => "https://www.rust-lang.org".to_string(),
+    };
+
+    // create request
+    let request = match std::env::args().nth(1) {
+        Some(cmd) => match cmd.as_str() {
+            "get" => client.get(addr),
+            _ => panic!("invalid request")
+        }
+        None => client.get(addr),
+    };
+
     // run get request
-    let body = client.get(addr).send().await?.text().await?;
+    let body = request.send().await?.text().await?;
 
     // print returned body
     println!("body = {:?}", body);
