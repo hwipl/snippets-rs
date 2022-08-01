272bff113705c17dc7fd971c1e87da041772b5a5
hwipl <33433250+hwipl@users.noreply.github.com>
2022-08-02 00:17:50 +0200

    surf/client: add request command line argument
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- surf/client/src/main.rs
+++ surf/client/src/main.rs
@@ -3,19 +3,28 @@ use surf::{Client, Config, Result};
 
 #[async_std::main]
 async fn main() -> Result<()> {
-    // get url from command line
-    let addr = match std::env::args().nth(1) {
-        Some(addr) => addr,
-        None => "https://www.rust-lang.org".to_string(),
-    };
-
     // create client
     let client: Client = Config::new()
         .set_timeout(Some(Duration::from_secs(5)))
         .try_into()?;
 
-    // run get request
-    let mut res = client.get(addr).await?;
+    // get url from command line
+    let addr = match std::env::args().nth(2) {
+        Some(addr) => addr,
+        None => "https://www.rust-lang.org".to_string(),
+    };
+
+    // create request
+    let request = match std::env::args().nth(1) {
+        Some(cmd) => match cmd.as_str() {
+            "get" => client.get(addr),
+            _ => panic!("invalid request"),
+        },
+        None => client.get(addr),
+    };
+
+    // wait for response
+    let mut res = request.await?;
 
     // print status
     let status = res.status();
