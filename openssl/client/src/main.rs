339602ef0be71cac9477d52e57923b4f5fd63437
hwipl <33433250+hwipl@users.noreply.github.com>
2022-08-06 02:20:55 +0200

    openssl/client: add error handling
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- openssl/client/src/main.rs
+++ openssl/client/src/main.rs
@@ -1,15 +1,18 @@
 use openssl::ssl::{SslConnector, SslMethod};
+use std::error::Error;
 use std::io::{Read, Write};
 use std::net::TcpStream;
 
-fn main() {
-    let connector = SslConnector::builder(SslMethod::tls()).unwrap().build();
+fn main() -> Result<(), Box<dyn Error>> {
+    let connector = SslConnector::builder(SslMethod::tls())?.build();
 
-    let stream = TcpStream::connect("google.com:443").unwrap();
-    let mut stream = connector.connect("google.com", stream).unwrap();
+    let stream = TcpStream::connect("google.com:443")?;
+    let mut stream = connector.connect("google.com", stream)?;
 
-    stream.write_all(b"GET / HTTP/1.0\r\n\r\n").unwrap();
+    stream.write_all(b"GET / HTTP/1.0\r\n\r\n")?;
     let mut res = vec![];
-    stream.read_to_end(&mut res).unwrap();
+    stream.read_to_end(&mut res)?;
     println!("{}", String::from_utf8_lossy(&res));
+
+    Ok(())
 }
