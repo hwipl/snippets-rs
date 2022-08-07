1baa9869c8757715ef7bde17bcfe0a16cf29d924
hwipl <33433250+hwipl@users.noreply.github.com>
2022-08-07 02:01:56 +0200

    openssl/client: get address from command line
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- openssl/client/src/main.rs
+++ openssl/client/src/main.rs
@@ -4,11 +4,18 @@ use std::io::{Read, Write};
 use std::net::TcpStream;
 
 fn main() -> Result<(), Box<dyn Error>> {
+    // get address from command line
+    let addr = match std::env::args().nth(1) {
+        Some(addr) => addr,
+        None => "www.rust-lang.org".to_string(),
+    };
+
+    // connect to server
     let connector = SslConnector::builder(SslMethod::tls())?.build();
+    let stream = TcpStream::connect(format!("{}:443", addr))?;
+    let mut stream = connector.connect(addr.as_str(), stream)?;
 
-    let stream = TcpStream::connect("google.com:443")?;
-    let mut stream = connector.connect("google.com", stream)?;
-
+    // run http request
     stream.write_all(b"GET / HTTP/1.0\r\n\r\n")?;
     let mut res = vec![];
     stream.read_to_end(&mut res)?;
