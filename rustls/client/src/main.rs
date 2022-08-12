ccaebe694c1b9dac0e720eae6899336d37306576
hwipl <33433250+hwipl@users.noreply.github.com>
2022-08-13 01:42:03 +0200

    rustls/client: cleanups
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- rustls/client/src/main.rs
+++ rustls/client/src/main.rs
@@ -5,19 +5,24 @@ use std::net::TcpStream;
 use std::sync::Arc;
 
 fn main() -> Result<(), Box<dyn Error>> {
+    // load certificates
     let mut roots = rustls::RootCertStore::empty();
     for cert in rustls_native_certs::load_native_certs()? {
         roots.add(&rustls::Certificate(cert.0))?;
     }
 
+    // create config
     let config = rustls::ClientConfig::builder()
         .with_safe_defaults()
         .with_root_certificates(roots)
         .with_no_client_auth();
 
+    // connect to server
     let mut conn = rustls::ClientConnection::new(Arc::new(config), "google.com".try_into()?)?;
     let mut sock = TcpStream::connect("google.com:443")?;
     let mut tls = rustls::Stream::new(&mut conn, &mut sock);
+
+    // send http request
     tls.write_all(
         concat!(
             "GET / HTTP/1.1\r\n",
@@ -28,6 +33,8 @@ fn main() -> Result<(), Box<dyn Error>> {
         )
         .as_bytes(),
     )?;
+
+    // check cipher suite
     let ciphersuite = tls
         .conn
         .negotiated_cipher_suite()
@@ -37,6 +44,8 @@ fn main() -> Result<(), Box<dyn Error>> {
         "Current ciphersuite: {:?}",
         ciphersuite.suite()
     )?;
+
+    // get http response
     let mut plaintext = Vec::new();
     tls.read_to_end(&mut plaintext)?;
     stdout().write_all(&plaintext)?;
