366bdd7bbbc3972dd6eaa905c9a45d38a478707d
hwipl <33433250+hwipl@users.noreply.github.com>
2023-04-01 13:39:36 +0200

    libp2p/request: update dependencies
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- libp2p/request/src/main.rs
+++ libp2p/request/src/main.rs
@@ -6,7 +6,7 @@ use futures::prelude::*;
 use libp2p::core::upgrade::{read_length_prefixed, write_length_prefixed};
 use libp2p::core::ProtocolName;
 use libp2p::request_response::{Behaviour, Codec, Config, Event, Message, ProtocolSupport};
-use libp2p::swarm::{Swarm, SwarmEvent};
+use libp2p::swarm::{Swarm, SwarmBuilder, SwarmEvent};
 use libp2p::{identity, Multiaddr, PeerId};
 use std::error::Error;
 use std::task::Poll;
@@ -110,7 +110,8 @@ fn main() -> Result<(), Box<dyn Error>> {
     let behaviour = Behaviour::new(HelloCodec(), protocols.clone(), cfg.clone());
 
     // create swarm
-    let mut swarm = Swarm::with_async_std_executor(transport, behaviour, local_peer_id);
+    let mut swarm =
+        SwarmBuilder::with_async_std_executor(transport, behaviour, local_peer_id).build();
 
     // listen on loopback interface and random port.
     swarm.listen_on("/ip6/::1/tcp/0".parse()?)?;
