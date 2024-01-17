e6dc70007cda6cc67ef9c6189121011191297f29
hwipl <33433250+hwipl@users.noreply.github.com>
2024-01-17 10:12:35 +0100

    add actix-web/greet
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

---
+++ actix-web/greet/src/main.rs
@@ -0,0 +1,41 @@
+use actix_web::{get, web, App, HttpServer, Responder};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    "Hello, world!"
+}
+
+#[get("/hi")]
+async fn hi() -> impl Responder {
+    format!("hi")
+}
+
+#[get("/hi/{name}")]
+async fn hi_name(name: web::Path<String>) -> impl Responder {
+    format!("hi {name}!")
+}
+
+#[get("/bye")]
+async fn bye() -> impl Responder {
+    format!("bye")
+}
+
+#[get("/bye/{name}")]
+async fn bye_name(name: web::Path<String>) -> impl Responder {
+    format!("bye {name}!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(index)
+            .service(hi)
+            .service(hi_name)
+            .service(bye)
+            .service(bye_name)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
