248a70275df176f644233ea111765931212a5798
hwipl <33433250+hwipl@users.noreply.github.com>
2024-04-06 14:05:19 +0200

    whoami: update dependencies
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- whoami/src/main.rs
+++ whoami/src/main.rs
@@ -1,14 +1,21 @@
 // whoami examples based on whoami docs
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("whoami::realname():    {}", whoami::realname());
     println!("whoami::username():    {}", whoami::username());
     println!(
-        "whoami::lang():        {:?}",
-        whoami::lang().collect::<Vec<String>>()
+        "whoami::langs():        {:?}",
+        whoami::langs()?
+            .map(|l| l.to_string())
+            .collect::<Vec<String>>()
     );
     println!("whoami::devicename():  {}", whoami::devicename());
-    println!("whoami::hostname():    {}", whoami::hostname());
+    println!(
+        "whoami::fallible::hostname():    {}",
+        whoami::fallible::hostname()?
+    );
     println!("whoami::platform():    {}", whoami::platform());
     println!("whoami::distro():      {}", whoami::distro());
     println!("whoami::desktop_env(): {}", whoami::desktop_env());
+
+    Ok(())
 }
