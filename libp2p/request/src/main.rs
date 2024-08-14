8018ef08c7697f2708cb48f5009f8fe768d24024
hwipl <33433250+hwipl@users.noreply.github.com>
2024-08-14 09:32:50 +0200

    libp2p/request: update dependencies
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- libp2p/request/src/main.rs
+++ libp2p/request/src/main.rs
@@ -95,17 +95,14 @@ struct HelloResponse(Vec<u8>);
 
 fn main() -> Result<(), Box<dyn Error>> {
     // create swarm
-    let builder = block_on(
-        SwarmBuilder::with_new_identity()
-            .with_async_std()
-            .with_tcp(
-                Default::default(),
-                (libp2p::tls::Config::new, libp2p::noise::Config::new),
-                libp2p::yamux::Config::default,
-            )?
-            .with_dns(),
-    )?;
-    let mut swarm = builder
+    let mut swarm = SwarmBuilder::with_new_identity()
+        .with_async_std()
+        .with_tcp(
+            Default::default(),
+            (libp2p::tls::Config::new, libp2p::noise::Config::new),
+            libp2p::yamux::Config::default,
+        )?
+        .with_dns()?
         .with_behaviour(|_key| {
             // create request response network behaviour
             let protocols = iter::once((HelloProtocol(), ProtocolSupport::Full));
