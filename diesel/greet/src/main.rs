9c67ba6ad08d5ff7b479eba4c1da4851e01bd816
hwipl <33433250+hwipl@users.noreply.github.com>
2025-07-21 09:19:44 +0200

    diesel/greet: add command line arguments
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- diesel/greet/src/main.rs
+++ diesel/greet/src/main.rs
@@ -1,6 +1,15 @@
 use diesel::prelude::*;
 use greet::*;
 
+fn print_usage() {
+    print! {"Usage:
+  run                   run some test commands
+  list                  list greetings
+  id <id>               get greeting by id
+  text <greeting>       get greeting by text
+"}
+}
+
 fn main() -> diesel::QueryResult<()> {
     use greet::schema::greetings::dsl::greetings;
 
@@ -17,20 +26,45 @@ fn main() -> diesel::QueryResult<()> {
         last_id = greeting.id;
     }
 
-    // list greetings
-    list_greetings(connection);
+    // command line arguments
+    match std::env::args().nth(1).as_deref() {
+        Some("run") => {
+            // list greetings
+            list_greetings(connection);
 
-    // get greeting by id
-    get_id(connection, last_id);
+            // get greeting by id
+            get_id(connection, last_id);
 
-    // get greeting by text
-    get_text(connection, "good day");
+            // get greeting by text
+            get_text(connection, "good day");
 
-    // delete greeting by id
-    delete_id(connection, last_id);
+            // delete greeting by id
+            delete_id(connection, last_id);
 
-    // list greetings
-    list_greetings(connection);
+            // list greetings
+            list_greetings(connection);
+        }
+        Some("list") => {
+            list_greetings(connection);
+        }
+        Some("id") => {
+            if let Some(s) = std::env::args().nth(2)
+                && let Ok(id) = s.parse::<i32>()
+            {
+                get_id(connection, id);
+            } else {
+                print_usage()
+            }
+        }
+        Some("text") => {
+            if let Some(text) = std::env::args().nth(2) {
+                get_text(connection, &text);
+            } else {
+                print_usage()
+            }
+        }
+        _ => print_usage(),
+    }
 
     Ok(())
 }
