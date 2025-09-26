40d0d5dfd7172b0bfa7b18eb5648f9d99d84d762
hwipl <33433250+hwipl@users.noreply.github.com>
2025-09-26 11:22:10 +0200

    nats/greet-async: add json message
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- nats/greet-async/src/main.rs
+++ nats/greet-async/src/main.rs
@@ -1,9 +1,21 @@
 use futures_util::StreamExt;
+use serde::{Deserialize, Serialize};
 use tokio::select;
 use tokio::time::{self, Duration, Instant};
 
+/// Greeting message format
+#[derive(Serialize, Deserialize, Debug)]
+struct Greeting {
+    from: String,
+    to: String,
+    text: String,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), async_nats::Error> {
+    // get name from command line arguments
+    let name = std::env::args().nth(1).unwrap_or("client".to_string());
+
     // connect to server
     let client = async_nats::connect("nats://localhost:4222").await?;
 
@@ -19,7 +31,13 @@ async fn main() -> Result<(), async_nats::Error> {
             // handle timer events
             _ = &mut timer => {
                 // publish message
-                client.publish("hi", "hi".into()).await?;
+                let g = Greeting {
+                    from: name.clone(),
+                    to: "".to_owned(),
+                    text: "hi".to_owned(),
+                };
+                let j = serde_json::to_string(&g)?;
+                client.publish("hi", j.into()).await?;
 
                 // reset timer
                 timer.as_mut().reset(Instant::now() + Duration::new(5,0));
@@ -27,7 +45,19 @@ async fn main() -> Result<(), async_nats::Error> {
 
             // handle messages
             Some(message) = subscriber.next() => {
-                println!("Received message {:?}", message);
+                let g: Greeting = serde_json::from_slice(&message.payload)?;
+                println!("Received message {:?}", g);
+
+                // reply
+                if g.from != name && g.to == "" {
+                    let r = Greeting {
+                        from: name.clone(),
+                        to: g.from,
+                        text: "hi".to_owned(),
+                    };
+                    let j = serde_json::to_string(&r)?;
+                    client.publish("hi", j.into()).await?;
+                }
             }
         }
     }
