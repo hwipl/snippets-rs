ae5129a43e51a221ccf1af9c3dbf9e3add1fc955
hwipl <33433250+hwipl@users.noreply.github.com>
2025-10-01 09:54:59 +0200

    add socket/udp-async
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

---
+++ socket/udp-async/src/main.rs
@@ -0,0 +1,52 @@
+use tokio::net::UdpSocket;
+
+async fn run_server() -> std::io::Result<()> {
+    let socket = UdpSocket::bind("127.0.0.1:0").await?;
+
+    println!("Server listening on: {}", socket.local_addr()?);
+    let mut buf = [0; 2048];
+    loop {
+        let (len, src) = socket.recv_from(&mut buf).await?;
+        socket.send_to(&buf[..len], &src).await?;
+    }
+}
+
+async fn run_client() -> std::io::Result<()> {
+    // get server address from command line
+    let addr = std::env::args().nth(2).ok_or(std::io::Error::new(
+        std::io::ErrorKind::InvalidInput,
+        "invalid server address",
+    ))?;
+    let socket = UdpSocket::bind((std::net::Ipv4Addr::UNSPECIFIED, 0)).await?;
+    socket.connect(addr).await?;
+
+    // sent request
+    let request = b"hello world";
+    socket.send(request).await?;
+    println!(
+        "Sent request to server: {}",
+        String::from_utf8_lossy(request)
+    );
+
+    // read reply
+    let mut reply = vec![0; request.len()];
+    socket.recv(&mut reply).await?;
+    println!(
+        "Read reply from server: {}",
+        String::from_utf8_lossy(&reply)
+    );
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    // handle command line arguments
+    if let Some(cmd) = std::env::args().nth(1) {
+        match cmd.as_str() {
+            "server" => return run_server().await,
+            "client" => return run_client().await,
+            _ => (),
+        }
+    }
+    Ok(())
+}
